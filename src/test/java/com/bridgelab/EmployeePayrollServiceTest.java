4d3961411b2de908ffcdcbec039dd46a5f947ad3
pitu <ashishkumarvish.432@gmail.com>
2021-11-19 17:27:35 +0530

    To retrive all employee data starting from prticular date

--- src/test/java/com/bridgelab/EmployeePayrollServiceTest.java
+++ src/test/java/com/bridgelab/EmployeePayrollServiceTest.java
@@ -1,36 +1,52 @@
 package com.bridgelab;
-import com.bridgelabz.EmployeePayrollDBService;
-import com.bridgelabz.EmployeePayrollData;
-import com.bridgelabz.EmployeePayrollException;
-import com.bridgelabz.EmployeePayrollService;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.junit.jupiter.api.Assertions;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 import java.util.List;
 
-import static com.bridgelabz.EmployeePayrollService.IOService.DB_IO;
+import static com.bridgelab.EmployeePayrollService.IOService.DB_IO;
 
 public class EmployeePayrollServiceTest {
     EmployeePayrollService employeePayrollService = null;
-    @Before
+
+    @BeforeEach
     void setUp() {
         employeePayrollService = new EmployeePayrollService();
-
     }
+
     @Test
-    void  givenEmployeePayrolInDB_WhenRetrieved_ShouldMatchEmployeeCount() {
-        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+    void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() throws EmployeePayrollException {
         List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData(DB_IO);
         Assertions.assertEquals(4, employeePayrollData.size());
-
     }
+
     @Test
-    void givenEmployeePayrolInDB_WhenRetrieved_ShouldSyncWithDB() throws EmployeePayrollException {
+    void givenNewSalaryForEmployee_WhenUpdated_ShouldSyncWithDB() throws EmployeePayrollException {
         List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData(DB_IO);
-        employeePayrollService.updateEmployeeSalary("Terisa",50000.00);
-        boolean result = employeePayrollService.checkEmployeePayrollSyncWithDB("Terisa");
-        Assertion.assertTrue(result);
+        employeePayrollService.updateEmployeeSalary("Terisa", 500000.00);
+        boolean result = employeePayrollService.checkEmployeePayrollInSyncWithDB("Terisa");
+        Assertions.assertTrue(result);
     }
 
+    @Test
+    void givenNewSalaryForEmployee_WhenUpdated_ShouldSyncWithDB_UsingPreparedStatement()
+            throws EmployeePayrollException {
+        List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData(DB_IO);
+        employeePayrollService.updateEmployeeSalaryUsingPreparedStatement("Terisa", 300000.00);
+        boolean result = employeePayrollService.checkEmployeePayrollInSyncWithDB("Terisa");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    void givenDateRange_WhenRetrieved_ShouldMatchEmployeeCount() throws EmployeePayrollException {
+        employeePayrollService.readEmployeePayrollData(DB_IO);
+        LocalDate startDate = LocalDate.of(2018, 01, 01);
+        LocalDate endDate = LocalDate.now();
+        List<EmployeePayrollData> employeePayrollData = employeePayrollService
+                .readEmployeePayrollDataForDateRange(DB_IO, startDate, endDate);
+        Assertions.assertEquals(4, employeePayrollData.size());
+    }
 }
